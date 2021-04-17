efe15f44d0892a21a4a2143ae8f7521d703f2237
aminaIsTrue <aamina.ouj@gmail.com>
2021-04-17 19:28:48 +0200

    using function based views to create Django objects and retrieve them with list_view

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
 
     path('create/',views.create_view, name = 'create-path'),
+     path('',views.list_view, name = 'home-path'),
 
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -8,5 +8,18 @@ from .models import Post
 #create view
 def create_view(request):
 
+    if request.method == 'POST':
+
+        obj = PostForm(request.POST)
+        if obj.is_valid():
+            obj.save()
 
     return render(request, 'blog/create.html', {'form' : PostForm()})
+
+
+#list view
+
+def list_view(request):
+
+
+    return render(request, 'blog/list.html', {'posts': Post.objects.all()})
