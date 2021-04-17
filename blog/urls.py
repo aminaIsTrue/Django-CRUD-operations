2a4edaaa13b06f665351565c810b0fbfee7c891a
aminaIsTrue <aamina.ouj@gmail.com>
2021-04-17 20:16:09 +0200

    1. creatinf the Update and delete  views using FBVs

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
 
     path('create/',views.create_view, name = 'create-path'),
      path('',views.list_view, name = 'home-path'),
+     path('list/<int:post_pk>',views.detail_view, name = 'detail-path'),
+     path('update/<int:post_pk>',views.update_view, name = 'update-path'),
+     path('delete/<int:post_pk>',views.delete_view, name = 'delete-path'),
 
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from .forms import PostForm
 from .models import Post
@@ -13,6 +13,7 @@ def create_view(request):
         obj = PostForm(request.POST)
         if obj.is_valid():
             obj.save()
+            return redirect('home-path')
 
     return render(request, 'blog/create.html', {'form' : PostForm()})
 
@@ -23,3 +24,40 @@ def list_view(request):
 
 
     return render(request, 'blog/list.html', {'posts': Post.objects.all()})
+
+
+
+#detail view
+
+def detail_view(request, post_pk):
+
+
+    return render(request, 'blog/detail.html', {'post': Post.objects.get(id = post_pk)})
+
+
+
+# update view
+
+def update_view(request, post_pk):
+
+    if request.method == 'POST':
+        obj = PostForm(request.POST, instance = Post.objects.get(id = post_pk))
+        if obj.is_valid():
+            obj.save()
+            return redirect('home-path')
+
+    return render(request, 'blog/update.html', {'form' : PostForm(instance = Post.objects.get(id = post_pk))})
+
+
+
+# delete view
+
+def delete_view(request, post_pk):
+
+
+
+    obj = Post.objects.get(id = post_pk)
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('home-path')
+    return render(request, 'blog/delete.html', {'post' : Post.objects.get(id = post_pk) })
